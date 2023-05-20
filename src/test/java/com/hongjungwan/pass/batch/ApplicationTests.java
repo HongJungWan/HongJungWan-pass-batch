4403bb4be7c4ec10c4462cb2074eeaa184a16fd1
HongJungWan <hjw43ok@hs.ac.kr>
2023-05-21 00:10:04 +0900

    init project

---
+++ src/test/java/com/hongjungwan/pass/batch/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongjungwan.pass.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
